a3e9b3ef764eb063ca16cc1a7e29ba41b3f4945a
Ali zaib <alizaib12220@gmail.com>
2024-11-19 18:02:49 +0500

    project file ,folder and Route setup

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function adminDashboard(Request $request){
+        $data = [
+            'pageTitle' => 'Dashboard'
+        ];
+
+        return view('back.pages.dashboard', $data);
+    }
+}
